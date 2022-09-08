1f823b78bb8a38d291c1e68879ac4c28743cbb90
Riyanadi120802 <Riyanadi083@gmail.com>
2022-09-08 20:42:20 +0700

    add function

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    data1 = int(input("Masukkan inputan 1 : "))
+    data2 = int(input("Masukkan inputan 2 : "))
+    data3 = int(input("Masukkan inputan 3 : "))
+
+
+    print("{}{}{}", format(data1, data2, data3))
+
+if __name__ == "__main__":
+    print("Hello main!")
+    main()
